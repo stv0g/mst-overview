224e7f8c2d4c009392b26434c24711b999c5f27b
konstin <konstin@mailbox.org>
2020-03-24 17:46:16 +0100

    Small changes

---
+++ build.sh
@@ -0,0 +1,11 @@
+set -e
+
+rm -r dist
+poetry run ./main.py
+npm run build
+cd ../meine-stadt-transparent
+cp Readme.md docs/index.md
+mkdocs build --clean
+cp -r etc site/etc
+cd ../mst-overview
+cp -r ../meine-stadt-transparent/site dist/docs
