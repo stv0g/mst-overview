26eb076535114b7213905338ee91fae1f117f842
konstin <konstin@mailbox.org>
2020-06-27 12:46:37 +0200

    Add Bielefeld

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ poetry run ./main.py
 npm run build
 cd ../meine-stadt-transparent
 cp Readme.md docs/index.md
-mkdocs build --clean
+../mst-overview/.venv/bin/mkdocs build --clean
 cp -r etc site/etc
 cd ../mst-overview
 cp -r ../meine-stadt-transparent/site dist/docs
